ec96c079ab76bdc9cccb3e30573528cf0211ed0a
skandog <muumrar@gmail.com>
2023-01-15 14:45:26 +0000

    added functionality to restrict empty items

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import ToDo from "./ToDo";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your buuuuggomg app!</Text>
       <StatusBar style="auto" />
+      <ToDo />
     </View>
   );
 }
--- ToDo.js
+++ ToDo.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TextInput, Button, Alert } from "react-native";
 
 const ToDo = () => {
-  const [todo, setTodo] = useState('');
+  const [todo, setTodo] = useState("");
   const [todos, setTodos] = useState([]);
 
   const handleSubmit = () => {
+    if (!todo) {
+      Alert.alert("Error", "Please enter a to-do item");
+      return;
+    }
     setTodos([...todos, todo]);
-    setTodo('');
+    setTodo("");
   };
 
   return (
     <View>
       <TextInput
         value={todo}
-        onChangeText={text => setTodo(text)}
+        onChangeText={(text) => setTodo(text)}
         placeholder="Enter a to-do item"
       />
       <Button title="Add" onPress={handleSubmit} />
