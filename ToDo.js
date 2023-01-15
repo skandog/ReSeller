60476aab90dda88ee65b8d345907434500087691
skandog <muumrar@gmail.com>
2023-01-15 15:00:53 +0000

    reconfigured todo component to include dispatch to props

--- ToDo.js
+++ ToDo.js
@@ -1,39 +1,50 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
 import { connect } from "react-redux";
 
 const ToDo = (props) => {
-  const handleSubmit = () => {
-    if (!props.todo) {
-      Alert.alert("Error", "Please enter a to-do item");
-      return;
+    const handleSubmit = () => {
+      if (!props.todo) {
+        Alert.alert('Error', 'Please enter a to-do item');
+        return;
+      }
+      props.addTodo(props.todo);
+      props.updateTodo('');
+    };
+    const handleDelete = (index) => {
+      props.deleteTodo(index)
     }
-    props.addTodo(props.todo);
-    props.updateTodo("");
-  };
-
-  return (
-    <View>
-      <TextInput
-        value={props.todo}
-        onChangeText={(text) => props.updateTodo(text)}
-        placeholder="Enter a to-do item"
-      />
-      <Button title="Add" onPress={handleSubmit} />
+    return (
       <View>
-        {props.todos.map((item, index) => (
-          <Text key={index}>{item}</Text>
-        ))}
+        <TextInput
+          value={props.todo}
+          onChangeText={text => props.updateTodo(text)}
+          placeholder="Enter a to-do item"
+        />
+        <Button title="Add" onPress={handleSubmit} />
+        <View>
+          {props.todos.map((item, index) => (
+            <View key={index}>
+              <Text >{item}</Text>
+              <Button title="Delete" onPress={() => handleDelete(item)}/>
+            </View>
+          ))}
+        </View>
       </View>
-    </View>
-  );
-};
-
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos,
-    todo: state.todo,
+    );
   };
-};
-
-export default ToDo;
+  
+  const mapStateToProps = state => {
+    return {
+      todos: state.todos,
+      todo: state.todo,
+    };
+  };
+  
+  const mapDispatchToProps = {
+      addTodo: (todo) => ({type: 'ADD_TODO', payload: todo}),
+      updateTodo: (todo) => ({type: 'UPDATE_TODO', payload: todo}),
+      deleteTodo: (todo) => ({type: 'DELETE_TODO', payload: todo})
+  }
+  
+  export default connect(mapStateToProps, mapDispatchToProps)(ToDo);
