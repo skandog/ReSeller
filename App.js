0814dc16ab4a8c75f7fe406cd325f62fc83d9b87
skandog <muumrar@gmail.com>
2023-01-16 11:30:09 +0000

    swithced to flex direction

--- App.js
+++ App.js
@@ -28,11 +28,14 @@ export default function App() {
         style={{
           backgroundColor: "#fff",
           flex: 1,
+          flexDirection: "row",
         }}
       >
-        <View style={{ backgroundColor: "tomato", flex: 2 }} />
-        <View style={{ backgroundColor: "dodgerblue", flex: 1 }} />
-        <View style={{ backgroundColor: "gold", flex: 1 }} />
+        <View style={{ backgroundColor: "tomato", width: 100, height: 100 }} />
+        <View
+          style={{ backgroundColor: "dodgerblue", width: 100, height: 100 }}
+        />
+        <View style={{ backgroundColor: "gold", width: 100, height: 100 }} />
       </View>
     </Provider>
   );
